ea1b6dc00faec7b856d71fd163a2f63ac2b514f4
Topchy Alexey <topchyalexey@yandex-team.ru>
2020-01-01 15:00:24 +0300

    Initial commit

---
+++ precache-manifest.2fae25c4bb2a2367f1ee9c9644b261e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a76afbc7dd14ff3bdd0f4e2251ab08a",
+    "url": "/vk-app/index.html"
+  },
+  {
+    "revision": "eb210981e95487f562c0",
+    "url": "/vk-app/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "3b718ec25a414aa393eb",
+    "url": "/vk-app/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "eb210981e95487f562c0",
+    "url": "/vk-app/static/js/2.de658857.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/vk-app/static/js/2.de658857.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b718ec25a414aa393eb",
+    "url": "/vk-app/static/js/main.ab0ab6d4.chunk.js"
+  },
+  {
+    "revision": "1ca8ca7e64a517c89fc5",
+    "url": "/vk-app/static/js/runtime-main.478c7cd0.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-app/static/media/persik.4e1ec840.png"
+  }
+]);
